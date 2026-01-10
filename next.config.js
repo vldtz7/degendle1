9be108284eb6eb8d45b8ca2bbe88ab2f1c6629ce
vldtz7 <169705015+vldtz7@users.noreply.github.com>
2026-01-11 00:09:35 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Enable React Strict Mode for better development experience
-  reactStrictMode: true,
-};
-
-export default nextConfig;
